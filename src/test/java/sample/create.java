700680c93cd377aa530be3958ab12de0aa18bb97
Mallikarjun <Mallikarjun@DESKTOP-KA79GL8>
2022-10-02 11:35:58 +0530

    1st commit

---
+++ src/test/java/sample/create.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class create 
+{
+@Test
+public void add()
+{
+	int a=4;
+	int b=6;
+	int c=a+b;
+	System.out.println("value is"+c );
+}
+}
